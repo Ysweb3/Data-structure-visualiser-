4886c63d4e11fcf1c7e7f9a09c7eb11dc975b136
yash <yssh88972@gmail.com>
2025-11-06 01:07:37 +0530

    add basic canvas logic,Todo:DPR scaling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import "./styles.css";
 
+const canvas = document.getElementById("viz");
 
-console.log("Hello World!");
+const ctx = canvas.getContext("2d");
+
+if (!ctx){
+    console.log("NULL")
+}
+else{
+    console.log(ctx);
+}
+ctx.fillStyle = "black";
+let x=20,y=30,h=60,w=100;
+let gap = 120;
+for(let i = 0;i<4;i++){
+    ctx.fillRect(x,y,w,h); 
+    x=x+gap;
+    console.log(x,y)
+}
